d42b4f91c640f3b7afe4860627d8ff140a8fe498
Isaacluka <lukaisaacabdulkarim@gmail.com>
2026-02-18 11:54:24 +0100

    Added Test video hosted on Youtube

--- src/components/CinematicPlayer.jsx
+++ src/components/CinematicPlayer.jsx
@@ -5,11 +5,26 @@ const CinematicPlayer = () => {
     const { name } = useParams();  
 
   return (
-    <Link to="/postscreening">
-        <div>CinematicPlayer</div>
-    </Link>
-    
+    <>
+      {/* <Link to="/postscreening">
+          <div>CinematicPlayer</div>
+      </Link> */}
+      <div className="min-h-screen bg-black flex items-center justify-center">
+        <div className="w-full max-w-7xl aspect-video shadow-2xl">
+          <iframe
+            className="w-full h-full rounded-lg"
+            src="https://www.youtube.com/embed/Q8q3EVcaFqY?modestbranding=1&rel=0"
+            title="NOIR Cinema Film"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </>    
   )
 }
+
+
 
 export default CinematicPlayer
