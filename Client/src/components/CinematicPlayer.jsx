99982ad4335892803b40211fe2051c63ccc17bdf
Isaacluka <lukaisaacabdulkarim@gmail.com>
2026-02-24 01:28:42 +0100

    Started Backend and create auth for access page

---
+++ Client/src/components/CinematicPlayer.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { useRef } from "react";
+import { Link, useParams } from 'react-router'
+import YouTube from "react-youtube";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
+
+
+const CinematicPlayer = () => {
+    // const { name } = useParams();  
+    const navigate = useNavigate();
+    const containerRef = useRef();
+
+    const handleReady = (event) => {
+    // Auto play
+      event.target.playVideo();
+    };
+
+    const handleStateChange = (event) => {
+      // When video starts playing
+      if (event.data === 1) {
+        if (containerRef.current.requestFullscreen) {
+          containerRef.current.requestFullscreen();
+        }
+      }
+      if (event.data === 0) {
+        navigate("/postScreening");
+      }
+    };
+
+    
+    const handleEnd = () =>{
+      navigate("/postScreening");
+    };
+
+    useEffect(() => {
+      window.history.pushState(null, "", window.location.href);
+      window.onpopstate = function () {
+        window.history.go(1);
+    };
+}, []);
+
+  return (
+    <>
+      {/* <Link to="/postscreening">
+          <div>CinematicPlayer</div>
+      </Link> */}
+      <div className="min-h-screen bg-black flex items-center justify-center">
+        <div className="w-full max-w-7xl aspect-video shadow-2xl">
+          <YouTube 
+            className="w-full h-full rounded-lg"
+            videoId = "Q8q3EVcaFqY"
+            onReady={handleReady}
+            onStateChange={handleStateChange}
+            onEnd = {handleEnd}
+            opts = {{
+              width:"100%",
+              height: "100%",
+              playerVars: {
+                modestbranding: 1,
+                rel:0,
+              },
+            }}
+          />
+
+
+          {/* <iframe
+            className="w-full h-full rounded-lg"
+            src="https://www.youtube.com/embed/Q8q3EVcaFqY?modestbranding=1&rel=0"
+            title="NOIR Cinema Film"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe> */}
+        </div>
+      </div>
+    </>    
+  )
+}
+
+
+
+export default CinematicPlayer
---
+++ Server/Server.js
@@ -0,0 +1,31 @@
+// server.js
+import express from "express";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const SECRET = "noir_super_secret";
+
+// Fake database (replace with MongoDB later)
+const validPins = ["12345678", "87654321"];
+
+app.post("/api/verify", (req, res) => {
+  const { pin } = req.body;
+
+  if (!pin) {
+    return res.status(400).json({ message: "PIN required" });
+  }
+
+  if (!validPins.includes(pin)) {
+    return res.status(401).json({ message: "Invalid access key" });
+  }
+
+  const token = jwt.sign({ access: true }, SECRET, { expiresIn: "2h" });
+
+  res.json({ token });
+});
+
+app.listen(5000, () => console.log("Server running"));
